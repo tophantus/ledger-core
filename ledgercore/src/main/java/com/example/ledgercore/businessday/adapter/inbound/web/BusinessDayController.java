64b5ca7e2195dbbc10e44a83bc14edb738c1139d
tophantus <tophantus@gmail.com>
2026-09-07 15:56:08 +0700

    feat(backend-businessday): return api response when closing business day

--- ledgercore/src/main/java/com/example/ledgercore/businessday/adapter/inbound/web/BusinessDayController.java
+++ ledgercore/src/main/java/com/example/ledgercore/businessday/adapter/inbound/web/BusinessDayController.java
@@ -1,11 +1,10 @@
 package com.example.ledgercore.businessday.adapter.inbound.web;
 
 import com.example.ledgercore.businessday.command.port.inbound.CloseBusinessDayUseCase;
+import com.example.ledgercore.common.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/admin/business-days")
@@ -15,10 +14,15 @@ public class BusinessDayController {
     private final CloseBusinessDayUseCase closeBusinessDayUseCase;
 
     @PostMapping("/close")
-    public ResponseEntity<Void> closeBusinessDay() {
+    public ResponseEntity<ApiResponse<Void>> closeBusinessDay() {
 
         closeBusinessDayUseCase.execute();
 
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(
+                ApiResponse.success(
+                        null,
+                        "Business day closed successfully"
+                )
+        );
     }
 }
